07915687ba8fce828681499fe7b68f141fe5e6e6
Ae <hi@angel-estrada.com>
2024-01-09 11:14:24 -0800

    update-test

--- src/mongodb.rs
+++ src/mongodb.rs
@@ -31,26 +31,13 @@ pub fn get_mongodb_client() -> Result<&'static Client, Box<dyn Error + Send + Sy
     return MONGODB_CLIENT.get().ok_or_else(|| "Missing MongoDB client as static reference".into());
 }
 
-/*
 #[cfg(test)]
-use super::*;
-use mongodb::*;
-use mongodb::bson::{doc, Document};
-use serde_json::{to_string};
+use tokio;
 
 #[tokio::test]
 async fn test_create_mongodb_client() {
+    env::set_var(get_mongodb_uri_env_key(), "mongodb+srv://foo:bar@cluster0.irqdk.mongodb.net/test");
     create_mongodb_client().await;
 
-    let ip = "127.0.0.1";
-    let client = get_mongodb_client().unwrap();
-    let database = client.database("ae");
-    let collection: Collection<Document> = database.collection("logs");
-    let filter = doc! { "ip": ip };
-    let cursor = collection.find_one(filter, None).await.unwrap();
-    let output = to_string(&cursor).unwrap();
-
-    assert!(output.contains("ip"));
-    assert!(output.contains(ip));
+    assert!(MONGODB_CLIENT.get().is_some());
 }
-*/
